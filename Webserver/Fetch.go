6226591a9363f6967a7b247ef3018137af4baa23
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2022-08-07 10:57:30 +0530

    adding webserver

---
+++ Webserver/Fetch.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"io/ioutil"
+	"log"
+)
+
+func main() {
+	res, err := http.Get("http://www.Google.com")
+	CheckError(err)
+	data, err := ioutil.ReadAll(res.Body)
+	CheckError(err)
+	fmt.Printf("Got: %q", string(data))
+}
+
+func CheckError(err error){
+	if err != nil {
+		log.Fatalf("Get: %v", err)
+	}
+}
